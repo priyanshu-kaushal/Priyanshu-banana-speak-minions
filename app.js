703ffb31c6ea228ad94498170a45f93f4fb7fed8
priyanshu5020 <kaushalpriyanshu5020@gmail.com>
2022-07-05 00:10:01 +0530

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
@@ -17,7 +17,7 @@ function clickHandler() {
     var inputText = txtInput.value;
 
     fetch(getTranslationURL(inputText))
-        .then(response => response.json)
+        .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
             outputDiv.innerText = translatedText;
